a85cca23b3a7d57f169eb04ee07e03ac67b4e736
Risteel <39449057+Risteel@users.noreply.github.com>
2019-10-07 20:44:55 +0800

    update vernam

--- HW1/Encrypt/Vernam.cpp
+++ HW1/Encrypt/Vernam.cpp
@@ -1,5 +1,4 @@
 #include "Vernam.h"
-
 Vernam::Vernam(string key, string plaintext) : Encrypt(key, plaintext)
 {
 	Cipher();
@@ -7,13 +6,18 @@ Vernam::Vernam(string key, string plaintext) : Encrypt(key, plaintext)
 
 void Vernam::Cipher()
 {
+	if (key.length() < plaintext.length()) key += plaintext.substr(0,plaintext.length() - key.length());
+	for (int i = 0; i < key.length(); i++)
+	{
+		key[i] = toUpperCase(key[i]);
+	}
 	for (int i = 0; i < plaintext.length(); i++)
 	{
 		int temp;
 		if (isAlphabet(plaintext[i]))
 		{
 			temp = plaintext[i] - 'a';
-			ciphertext +=(temp ^ (key[i % key.length()] - 'A')) + 'A';
+			ciphertext += (temp ^ (key[i % key.length()] - 'A')) + 'A';
 		}
 		else
 			ciphertext += plaintext[i];
